8690949c989b6623a570eecc5bae12ea85745384
Bruno Garutti <garuttibruno03@gmail.com>
2026-03-03 17:23:38 -0300

    codigos

---
+++ Treino/codfigo 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a;]
+    printf("Digite o numero \n");
+    scanf("%d",&a);
+    
+    if ( a % 2 == 0 ){
+        printf("PAR \n");
+    } else {
+        printf("IMPAR \n");
+    }
+    return 0;
+}
