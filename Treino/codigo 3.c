250589c6cf7ce160270b6c04a65e533cdf480692
Bruno Garutti <garuttibruno03@gmail.com>
2026-03-03 17:30:30 -0300

    correção c3

---
+++ Treino/codigo 3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a; 
+    printf("Digite um valor dentre 1 a 100, e descubra qual time você pertence: \n")
+    scanf("%d",&a);
+    
+    if ( a >= 90 ){
+        printf("A \n");
+    } else if ( a >= 70 ){
+        printf("B \n");
+    } else if ( a >= 50 ){
+        printf("C \n");
+    } else if ( a < 50 ){
+        printf("D \n");
+    } else {
+        printf("Numero Invalido \n");
+    }
+    
+    return 0;
+}
