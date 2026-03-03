e3f99bd1d5a37e2e8e91f7cc31dc1ffe6edb1021
Bruno Garutti <garuttibruno03@gmail.com>
2026-03-03 17:32:06 -0300

    c4

---
+++ Treino/codigo 4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    int c;
+    printf("Digite o valor de A: \n")
+    scanf("%d",&a);
+    printf("Digite o valor de B: \n")
+    scanf("%d",&b);
+    printf("Digite o valor de C: \n")
+    scanf("%d",&c);
+    
+    if ( a + b > c && a + c > b && b + c > a ){
+        if ( a == b && b == c ){
+            printf("EQUILATERO \n");
+        } else if (( a == b && a != c) || ( a == c && a != b) || ( b == c && b != a )) {
+            printf("ISOSCELES \n");
+        } else if ( a != b && a != c && b != c ){
+            printf("ESCALENO \n");
+        }
+        
+    } else {
+        printf("NAO FORMA TRIANGULO \n");
+    }
+    return 0;
+}
