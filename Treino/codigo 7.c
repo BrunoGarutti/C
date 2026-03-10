e392cd9bd4da9f3d1ab36524b1e299e0f069907b
Bruno Garutti <garuttibruno03@gmail.com>
2026-03-10 17:10:56 -0300

    adc c7

---
+++ Treino/codigo 7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void organizavetor (int v[],int n){
+    int par = 0
+    int aux;
+
+    for ( int i = 0 ; i < n ; i++ ){
+        if ( v[i] % 2 == 0 ){
+            aux = v[];
+            v[] = v[par];
+            v[par] = aux;
+            
+            par++;
+        }
+    }
+}
+
+int main(){
+    int v[] = {3,5,2,8,1,10,4,7};
+    int n = 8
+
+    organizavetor(v,n);
+
+    printf("O vetor organziado fica: \n");
+    for (int i = 0 ; i < n ; i++ ){
+        printf("%d",v[i]);
+    }
+    printf("\n")
+
+    return 0;
+}
