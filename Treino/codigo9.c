2be8e57c936a299fcea76df79e91fde8d3704c30
Bruno Garutti <garuttibruno03@gmail.com>
2026-03-23 11:53:29 +0000

    printcod9

--- Treino/codigo9.c
+++ Treino/codigo9.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 
-void multmatriz(int m[3][3],int i,int j,int x){
-    for ( i = 0; i < 3; i++ ){
-        for ( j = 0; j < 3; j++ ){
+// Função que multiplica cada elemento da matriz por um valor x
+void multmatriz(int m[3][3], int x) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             m[i][j] *= x;
         }
     }
 }
 
+int main() {
+    // 1. Declaração e inicialização da matriz 3x3
+    int matriz[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    int multiplicador;
+
+    printf("Digite o valor para multiplicar a matriz: ");
+    scanf("%d", &multiplicador);
+
+    // 2. Chamada da função
+    multmatriz(matriz, multiplicador);
+
+    // 3. Exibição do resultado
+    printf("\nMatriz resultante:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d\t", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
